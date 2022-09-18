8e417be988bd5af353a3e4dbe2af5a0d43f6cf69
ChiragBangera <chiragbangera468@gmail.com>
2022-09-18 19:53:33 +0530

    dojo-blog

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div classname="content">
+        <h1>App Component</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
